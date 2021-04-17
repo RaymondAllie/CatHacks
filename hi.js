7173f990472df32be903e8e8db2a233a7c3321ba
RaymondAllie <73182219+RaymondAllie@users.noreply.github.com>
2021-04-17 00:08:11 -0400

    Update hi.js

--- hi.js
+++ hi.js
@@ -1,4 +1,4 @@
-
+/*
 const url = 'https://superheroapi.com/api/';
 const characterid = '414/';
 const access = '897434584441270/'
@@ -14,7 +14,7 @@ function render(item) {
 
 
 async function fun() {
-  /* const wordQuery = document.querySelector('#in').value;  { mode: 'no-cors'} */
+   const wordQuery = document.querySelector('#in').value;  { mode: 'no-cors'} 
 
   const endpoint = url + access + characterid + item;
   console.log(endpoint)
@@ -28,4 +28,19 @@ async function fun() {
   } catch(error){
     console.log(error)
   }
+}
+*/
+var url = "https://superheroapi.com/api/897434584441270/414/powerstats";
+
+var xhr = new XMLHttpRequest();
+function fun() {
+  xhr.open("GET", url);
+  
+  xhr.onreadystatechange = function () {
+     if (xhr.readyState === 4) {
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+     }};
+
+  xhr.send();
 }
